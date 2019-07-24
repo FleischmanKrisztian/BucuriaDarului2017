637ddf2d45c02ad6f9503905710eead90b88d215
Fleischman Krisztian <kriky_sk8@yahoo.com>
2019-07-24 13:47:05 +0300

    Update MongoDBContext.cs

--- Finalaplication/App_Start/MongoDBContext.cs
+++ Finalaplication/App_Start/MongoDBContext.cs
@@ -1,46 +1,27 @@
-﻿using Finalaplication.Models;
-using MongoDB.Bson.Serialization;
-using MongoDB.Driver;
+﻿using MongoDB.Driver;
 using System.Configuration;
+using System;
 
 namespace Finalaplication.App_Start
 {
     public class MongoDBContext
-    {   
+    {
         public IMongoDatabase database;
 
         public MongoDBContext()
         {
-            //BsonClassMap.RegisterClassMap<Beneficiary>(cm =>
-            //{
-            //    cm.AutoMap();
-            //    cm.SetIgnoreExtraElements(true);
-
-
-            //});
-
-            //BsonClassMap.RegisterClassMap<Sponsor>(m =>
-            //{
-            //    m.AutoMap();
-            //    m.SetIgnoreExtraElements(true);
-
-
-            //});
-
             try
             {
-                var mongoClient = new MongoClient("mongodb+srv://Krisztian:rock44ever@siemens-application-mtrya.mongodb.net/test?retryWrites=true&w=majority");
-                database = mongoClient.GetDatabase("VolMongo");
-
-              
-
+                string EnvServerAddress = Environment.GetEnvironmentVariable("mongoserver");
+                string EnvDatabaseName = Environment.GetEnvironmentVariable("databasename");
+                var mongoClient = new MongoClient(EnvServerAddress);
+                database = mongoClient.GetDatabase(EnvDatabaseName);
             }
             catch
             {
                 var client = new MongoClient();
-                database = client.GetDatabase("Incercareax");
-               
-            }   
+                database = client.GetDatabase("VolMongo");
+            }
         }
     }
 }
