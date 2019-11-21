532bbec647b74587a7a05a0f6c59dba9c7a54e09
Fleischman Krisztian <kriky_sk8@yahoo.com>
2019-11-21 14:25:48 +0200

    nothing but working final

--- CSVExporter/StringtoCsv/Methods.cs
+++ CSVExporter/StringtoCsv/Methods.cs
@@ -1,7 +1,6 @@
 ﻿using ChoETL;
 using CsvHelper;
 using Newtonsoft.Json;
-using System;
 using System.Data;
 using System.IO;
 using System.Text;
@@ -13,30 +12,29 @@ namespace CSVExporter.StringtoCsv
         public static string VolunteersToCSVFormat(string result)
         {
             string csvasstring = "";
-            //string csvasstring = "Firstname,Lastname,Birthdate,Gender,CNP,Occupation,Filed_of_activity,Desired_workplace,InActivity,HourCount,HasCar,HasDrivingLicence,Remark,District,City,Number,Street,MailAddres,PhoneNumber\n";
             csvasstring = csvasstring + jsontocsvlasttry(result);
             return csvasstring;
         }
 
         public static string BeneficiariesToCSVFormat(string result)
         {
-            string csvasstring = "Firstname,Lastname,Active,Weekly package, Canteen, Home Delivery Driver, HAS GDPR,District,City,Street,Number,CNP,Has ID, IDSerie, IDNr, IDEliberat, IdEliberator, IDAplication, IDInvestigation, IDContract, Number Of Portions, Last Time Active, Comments, Birthdate, Phone Number,Birth place, Studies, Profession, Occupation, Seniority In Workfield, Health State,Disability,Chronic Condition, Addictions, Health Insurance,Health Card, Married, Spouse Name,Has Home, Housing Type,Income,Expenses,Gender,Has Contract, Number Of Registration, Registration Date,Expiration Date\n";
-            csvasstring = csvasstring + jsonToCSV(result);
+            string csvasstring = "";
+            csvasstring = csvasstring + jsontocsvlasttry(result);
             return csvasstring;
         }
 
         public static string SponsorsToCSVFormat(string result)
         {
-            string csvasstring = "NameOfSponsor,PhoneNumber,MailAdress,HasContract,NumberOfRegistration,RegistrationDate,ExpirationDate,DateOfSponsorships,MoneyAmount,WhatGoods,GoodsAmount\n";
-            csvasstring = csvasstring + jsonToCSV(result);
+            string csvasstring = "";
+            csvasstring = csvasstring + jsontocsvlasttry(result);
 
             return csvasstring;
         }
 
         public static string EventsToCSVFormat(string result)
         {
-            string csvasstring = "NameOfEvent,DateOfEvent,Duration,NumberOfVolunteersNeeded,PlaceOfEvent,TypeOfActivities,TypeOfEvent,AllocatedVolunteers,AllocatedSponsors\n";
-            csvasstring = csvasstring + jsonToCSV(result);
+            string csvasstring = "";
+            csvasstring = csvasstring + jsontocsvlasttry(result);
             return csvasstring;
         }
 
@@ -61,31 +59,6 @@ namespace CSVExporter.StringtoCsv
             return csv.ToString();
         }
 
-        //public static string jsonToCSV(string jsonContent)
-        //{
-        //    StringWriter csvString = new StringWriter();
-        //    using (var csv = new CsvWriter(csvString))
-        //    {
-        //        using (var dt = jsonStringToTable(jsonContent))
-        //        {
-        //            foreach (DataColumn column in dt.Columns)
-        //            {
-        //                csv.WriteField(column.ColumnName);
-        //            }
-        //            csv.NextRecord();
-
-        //            foreach (DataRow row in dt.Rows)
-        //            {
-        //                for (var i = 0; i < dt.Columns.Count; i++)
-        //                {
-        //                    csv.WriteField(row[i]);
-        //                }
-        //                csv.NextRecord();
-        //            }
-        //        }
-        //    }
-        //    return csvString.ToString();
-        //}
         public static string jsonToCSV(string jsonContent, string delimiter = ",")
         {
             StringWriter csvString = new StringWriter();
--- Finalaplication/App_Start/MongoDBContextOffline.cs
+++ Finalaplication/App_Start/MongoDBContextOffline.cs
@@ -12,14 +12,13 @@ namespace Finalaplication.App_Start
             var clientSettings = new MongoClientSettings
             {
                 Server = new MongoServerAddress(Environment.GetEnvironmentVariable(Common.VolMongoConstants.SERVER_NAME_SECONDARY),Int32.Parse(Environment.GetEnvironmentVariable(Common.VolMongoConstants.SERVER_PORT_SECONDARY))),
-                //Server = new MongoServerAddress("172.17.0.2",27017),
                 ClusterConfigurator = builder =>
                 {
                     builder.ConfigureCluster(settings => settings.With(serverSelectionTimeout: TimeSpan.FromSeconds(2)));
                 }
             };
             var client = new MongoClient(clientSettings);
-            databaseoffline = client.GetDatabase("BucuriaDaruluiOffline");
+            databaseoffline = client.GetDatabase(Environment.GetEnvironmentVariable(Common.VolMongoConstants.DATABASE_NAME_SECONDARY));
         }
     }
 }
