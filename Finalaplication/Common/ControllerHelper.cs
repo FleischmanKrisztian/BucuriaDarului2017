519ae94bace849c14bff9ed8bc92a53640858737
Fleischman Krisztian <kriky_sk8@yahoo.com>
2020-02-06 21:49:09 +0200

    beneficiary active date done

--- Finalaplication/Common/ControllerHelper.cs
+++ Finalaplication/Common/ControllerHelper.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 
@@ -26,6 +27,70 @@ namespace Finalaplication.Common
             {
                 return VolMongoConstants.DEFAULT_NUMBER_OF_ITEMS_PER_PAGE;
             }
+        }
+
+        public static (DateTime[] startdates, DateTime[] enddates, int i) Datereturner(string activedates)
+        {
+            DateTime[] startdates = new DateTime[20];
+            DateTime[] enddates = new DateTime[20];
+            int i = 0;
+
+            if (activedates != null)
+            {
+                while (activedates.Contains(","))
+                {
+                    bool last = false;
+                    int aux = activedates.IndexOf(",");
+                    activedates = activedates.Remove(0, 1);
+                    int end = activedates.IndexOf("-");
+                    int lastcharend = activedates.IndexOf(",");
+                    //in the case where there are are no dates left
+                    if (lastcharend == -1)
+                    {
+                        last = true;
+                        lastcharend = activedates.Length;
+                    }
+                    lastcharend = lastcharend - end;
+                    int lastcharstart = end - aux;
+                    string startdatestring = activedates.Substring(aux, lastcharstart);
+                    string enddatestring = activedates.Substring(lastcharstart + 1, lastcharend - 1);
+                    startdates[i] = Dateformatter(startdatestring);
+                    enddates[i] = Dateformatter(enddatestring);
+                    //checks if it was the last if it was it doesn't do the following steps not to break
+                    if (!last)
+                        activedates = activedates.Substring(activedates.IndexOf(','));
+
+                    i++;
+                }
+            }
+            Array.Resize(ref startdates, i);
+            Array.Resize(ref enddates, i);
+            return (startdates, enddates, i);
+        }
+
+        public static DateTime Dateformatter(string datestring)
+        {
+            DateTime date;
+            if (datestring.Contains("currently"))
+            {
+                date = DateTime.Today;
+                return date;
+            }
+            else if (datestring.Length == 8)
+            {
+                datestring = datestring.Insert(0, "0");
+                datestring = datestring.Insert(3, "0");
+            }
+            else if (datestring.Length == 9 && datestring[2] != '/')
+            {
+                datestring = datestring.Insert(0, "0");
+            }
+            else if (datestring.Length == 9)
+            {
+                datestring = datestring.Insert(2, "0");
+            }
+            date = DateTime.ParseExact(datestring, "dd/MM/yyyy", CultureInfo.DefaultThreadCurrentCulture);
+            return date;
         }
     }
 }
--- Finalaplication/Controllers/VolunteerController.cs
+++ Finalaplication/Controllers/VolunteerController.cs
@@ -418,71 +418,6 @@ namespace Finalaplication.Controllers
             }
         }
 
-
-        public (DateTime[] startdates, DateTime[] enddates, int i) Datereturner(string activedates)
-        {
-            DateTime[] startdates = new DateTime[20];
-            DateTime[] enddates = new DateTime[20];
-            int i = 0;
-
-            if (activedates != null)
-            {
-                while (activedates.Contains(","))
-                {
-                    bool last = false;
-                    int aux = activedates.IndexOf(",");
-                    activedates = activedates.Remove(0, 1);
-                    int end = activedates.IndexOf("-");
-                    int lastcharend = activedates.IndexOf(",");
-                    //in the case where there are are no dates left
-                    if (lastcharend == -1)
-                    {
-                        last = true;
-                        lastcharend = activedates.Length;
-                    }
-                    lastcharend = lastcharend - end;
-                    int lastcharstart = end - aux;
-                    string startdatestring = activedates.Substring(aux, lastcharstart);
-                    string enddatestring = activedates.Substring(lastcharstart + 1, lastcharend - 1);
-                    startdates[i] = Dateformatter(startdatestring);
-                    enddates[i] = Dateformatter(enddatestring);
-                    //checks if it was the last if it was it doesn't do the following steps not to break
-                    if (!last)
-                        activedates = activedates.Substring(activedates.IndexOf(','));
-
-                    i++;
-                }
-            }
-            Array.Resize(ref startdates, i);
-            Array.Resize(ref enddates, i);
-            return (startdates, enddates, i);
-        }
-
-        public DateTime Dateformatter(string datestring)
-        {
-            DateTime date;
-            if (datestring.Contains("currently"))
-            {
-                date = DateTime.Today;
-                return date;
-            }
-            else if (datestring.Length == 8)
-            {
-                datestring = datestring.Insert(0, "0");
-                datestring = datestring.Insert(3, "0");
-            }
-            else if (datestring.Length == 9 && datestring[2] != '/')
-            {
-                datestring = datestring.Insert(0, "0");
-            }
-            else if (datestring.Length == 9)
-            {
-                datestring = datestring.Insert(2, "0");
-            }
-            date = DateTime.ParseExact(datestring, "dd/MM/yyyy",CultureInfo.DefaultThreadCurrentCulture);
-            return date;
-        }
-
          public ActionResult Index(string lang,bool HasDrivingLicence,string searchedContact, string sortOrder, string searching, bool Active, bool HasCar, DateTime lowerdate, DateTime upperdate, DateTime activesince, DateTime activetill, int page,string gender,string searchedAddress,string searchedworkplace,string searchedOccupation,string searchedRemarks,int searchedHourCount)
 
         {
@@ -644,7 +579,7 @@ namespace Finalaplication.Controllers
                     string ids_to_remove = "";
                     foreach (Volunteer vol in volunteers)
                     {
-                        (DateTime[] startdates, DateTime[] enddates, int i) = Datereturner(vol.Activedates);
+                        (DateTime[] startdates, DateTime[] enddates, int i) = ControllerHelper.Datereturner(vol.Activedates);
                         bool passed = false;
                         for (int j = i - 1; j >= 0; j--)
                         {
@@ -672,7 +607,7 @@ namespace Finalaplication.Controllers
                     string ids_to_remove = "";
                     foreach (Volunteer vol in volunteers)
                     {
-                        (DateTime[] startdates, DateTime[] enddates, int i) = Datereturner(vol.Activedates);
+                        (DateTime[] startdates, DateTime[] enddates, int i) = ControllerHelper.Datereturner(vol.Activedates);
                         bool passed = false;
                         for (int j = i - 1; j >= 0; j--)
                         {
@@ -701,7 +636,7 @@ namespace Finalaplication.Controllers
 
                     foreach (Volunteer vol in volunteers)
                     {
-                        (DateTime[] startdates, DateTime[] enddates, int i) = Datereturner(vol.Activedates);
+                        (DateTime[] startdates, DateTime[] enddates, int i) = ControllerHelper.Datereturner(vol.Activedates);
                         bool passed = false;
                         for (int j = i - 1; j >= 0; j--)
                         {
