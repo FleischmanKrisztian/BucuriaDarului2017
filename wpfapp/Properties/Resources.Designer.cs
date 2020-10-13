f4453980e94b2b2e14550479435f679325156a8d
FleischmanKrisztian <fleischman.krisztian@gmail.com>
2020-10-13 17:50:07 +0300

    fixed

--- wpfapp/Properties/Resources.Designer.cs
+++ wpfapp/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace wpfapp.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap ContractImage {
+            get {
+                object obj = ResourceManager.GetObject("ContractImage", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap siemens4 {
             get {
                 object obj = ResourceManager.GetObject("siemens4", resourceCulture);
