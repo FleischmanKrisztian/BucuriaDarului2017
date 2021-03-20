1d37e64fb4dc6a0f48e641de2531191d7e775fbb
Gramada Corina <gramadacorina@gmail.com>
2021-03-20 15:30:41 +0200

    BeneficiaryManager done,added BeneficiaryFunctions

---
+++ Finalaplication/DatabaseManager/BeneficiaryManager.cs
@@ -0,0 +1,56 @@
+﻿using Finalaplication.App_Start;
+using Finalaplication.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Finalaplication.DatabaseManager
+{
+    public class BeneficiaryManager
+    {
+        private MongoDBContext dbcontext = new MongoDBContext();
+
+        internal void AddBeneficiaryToDB(Beneficiary beneficiary)
+        {
+            IMongoCollection<Beneficiary> beneficiarycollection = dbcontext.database.GetCollection<Beneficiary>("Beneficiaries");
+            try
+            {
+                beneficiarycollection.InsertOne(beneficiary);
+            }
+            catch
+            {
+                Console.WriteLine("There was an error adding Volunteer!");
+            }
+        }
+
+        internal Beneficiary GetOneBeneficiary(string id)
+        {
+            IMongoCollection<Beneficiary> beneficiarycollection = dbcontext.database.GetCollection<Beneficiary>("Beneficiaries");
+            var filter = Builders<Beneficiary>.Filter.Eq("_id", ObjectId.Parse(id));
+            Beneficiary volunteer = beneficiarycollection.Find(filter).FirstOrDefault();
+            return volunteer;
+        }
+        internal List<Beneficiary> GetListOfBeneficiaries()
+        {
+            IMongoCollection<Beneficiary> beneficiarycollection = dbcontext.database.GetCollection<Beneficiary>("Beneficiaries");
+            List<Beneficiary> beneficiaries = beneficiarycollection.AsQueryable().ToList();
+            return beneficiaries;
+        }
+
+        internal void UpdateVolunteer(FilterDefinition<Beneficiary> filter, UpdateDefinition<Beneficiary> beneficiarytoupdate)
+        {
+            IMongoCollection<Beneficiary> beneficiarycollection = dbcontext.database.GetCollection<Beneficiary>("Beneficiaries");
+            beneficiarycollection.UpdateOne(filter, beneficiarytoupdate);
+        }
+
+        internal void DeleteVolunteer(string id)
+        {
+            IMongoCollection<Beneficiary> beneficiarycollection = dbcontext.database.GetCollection<Beneficiary>("Beneficiaries");
+            beneficiarycollection.DeleteOne(Builders<Beneficiary>.Filter.Eq("_id", ObjectId.Parse(id)));
+        }
+
+    }
+}
