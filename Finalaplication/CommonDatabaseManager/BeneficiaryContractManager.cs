3ccac0190181e475e3ce0d3518f17c30af61c21c
Gramada Corina <gramadacorina@gmail.com>
2021-04-15 18:15:11 +0300

    BeneficiaryContractManager for Common Database

---
+++ Finalaplication/CommonDatabaseManager/BeneficiaryContractManager.cs
@@ -0,0 +1,64 @@
+﻿using Finalaplication.App_Start;
+using Finalaplication.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Finalaplication.CommonDatabaseManager
+{
+    public class BeneficiaryContractManager
+    {
+
+        private MongoDBContextCommon dbContextCommon = new MongoDBContextCommon();
+
+        internal void AddBeneficiaryContractToDB(Beneficiarycontract beneficiarycontract)
+        {
+            IMongoCollection<Beneficiarycontract> benecontractcollection = dbContextCommon.DatabaseCommon.GetCollection<Beneficiarycontract>("BeneficiariesContracts");
+            try
+            {
+                beneficiarycontract._id = Guid.NewGuid().ToString();
+                benecontractcollection.InsertOne(beneficiarycontract);
+            }
+            catch
+            {
+                Console.WriteLine("There was an error adding Beneficiary Contract");
+            }
+        }
+
+        internal Beneficiarycontract GetBeneficiaryContract(string id)
+        {
+            IMongoCollection<Beneficiarycontract> benecontractcollection = dbContextCommon.DatabaseCommon.GetCollection<Beneficiarycontract>("BeneficiariesContracts");
+            var filter = Builders<Beneficiarycontract>.Filter.Eq("_id", ObjectId.Parse(id));
+            Beneficiarycontract returnBeneficiaryContract = benecontractcollection.Find(filter).FirstOrDefault();
+            return returnBeneficiaryContract;
+        }
+
+        internal List<Beneficiarycontract> GetListOfBeneficiariesContracts()
+        {
+            IMongoCollection<Beneficiarycontract> benecontractcollection = dbContextCommon.DatabaseCommon.GetCollection<Beneficiarycontract>("BeneficiariesContracts");
+            List<Beneficiarycontract> contracts = benecontractcollection.AsQueryable().ToList();
+            return contracts;
+        }
+
+        internal void UpdateBeneficiaryContract(FilterDefinition<Beneficiarycontract> filter, UpdateDefinition<Beneficiarycontract> contract_toupdate)
+        {
+            IMongoCollection<Beneficiarycontract> benecontractcollection = dbContextCommon.DatabaseCommon.GetCollection<Beneficiarycontract>("BeneficiariesContracts");
+            benecontractcollection.UpdateOne(filter, contract_toupdate);
+        }
+
+        internal void DeleteBeneficiaryContract(string id)
+        {
+            IMongoCollection<Beneficiarycontract> benecontractcollection = dbContextCommon.DatabaseCommon.GetCollection<Beneficiarycontract>("BeneficiariesContracts");
+            benecontractcollection.DeleteOne(Builders<Beneficiarycontract>.Filter.Eq("_id", ObjectId.Parse(id)));
+        }
+
+        internal void DeleteAllContracts(string id)
+        {
+            IMongoCollection<Beneficiarycontract> benecontractcollection = dbContextCommon.DatabaseCommon.GetCollection<Beneficiarycontract>("BeneficiariesContracts");
+            benecontractcollection.DeleteMany(zzz => zzz.OwnerID == id);
+        }
+    }
+}
--- Finalaplication/CommonDatabaseManager/BeneficiaryManager.cs
+++ Finalaplication/CommonDatabaseManager/BeneficiaryManager.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Finalaplication.App_Start;
+using Finalaplication.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +11,51 @@ namespace Finalaplication.CommonDatabaseManager
 {
     public class BeneficiaryManager
     {
+        private MongoDBContextCommon dbContextCommon = new MongoDBContextCommon();
+
+        internal List<Beneficiary> GetListOfBeneficiaries()
+        {
+            IMongoCollection<Beneficiary> beneficiarycollection = dbContextCommon.DatabaseCommon.GetCollection<Beneficiary>("Beneficiaries");
+            List<Beneficiary> beneficiaries = beneficiarycollection.AsQueryable().ToList();
+            return beneficiaries;
+        }
+
+        internal void AddBeneficiaryToDB(Beneficiary beneficiary)
+        {
+            IMongoCollection<Beneficiary> beneficiarycollection = dbContextCommon.DatabaseCommon.GetCollection<Beneficiary>("Beneficiaries");
+            try
+            {
+                beneficiary._id = Guid.NewGuid().ToString();
+                beneficiarycollection.InsertOne(beneficiary);
+            }
+            catch
+            {
+                Console.WriteLine("There was an error adding Beneficiary!");
+            }
+        }
+
+        internal Beneficiary GetOneBeneficiary(string id)
+        {
+            IMongoCollection<Beneficiary> beneficiarycollection = dbContextCommon.DatabaseCommon.GetCollection<Beneficiary>("Beneficiaries");
+            var filter = Builders<Beneficiary>.Filter.Eq("_id", ObjectId.Parse(id));
+            Beneficiary beneficiary = beneficiarycollection.Find(filter).FirstOrDefault();
+            return beneficiary;
+        }
+
+
+        internal void UpdateABeneficiary(Beneficiary beneficiarytopdate, string id)
+        {
+            IMongoCollection<Beneficiary> Beneficiarycollection = dbContextCommon.DatabaseCommon.GetCollection<Beneficiary>("Beneficiaries");
+            var filter = Builders<Beneficiary>.Filter.Eq("_id", ObjectId.Parse(id));
+            beneficiarytopdate._id = id;
+            Beneficiarycollection.FindOneAndReplace(filter, beneficiarytopdate);
+        }
+
+        internal void DeleteBeneficiary(string id)
+        {
+            IMongoCollection<Beneficiary> beneficiarycollection = dbContextCommon.DatabaseCommon.GetCollection<Beneficiary>("Beneficiaries");
+            beneficiarycollection.DeleteOne(Builders<Beneficiary>.Filter.Eq("_id", ObjectId.Parse(id)));
+        }
+
     }
 }
