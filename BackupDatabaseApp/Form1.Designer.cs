64215905af3a4d736899731cc68b8d3834c2e6c5
Gramada Corina <gramadacorina@gmail.com>
2021-04-29 18:13:06 +0300

    Added some frontend elements

--- BackupDatabaseApp/Form1.Designer.cs
+++ BackupDatabaseApp/Form1.Designer.cs
@@ -28,25 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(169, 202);
+            this.button1.Location = new System.Drawing.Point(85, 251);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(101, 29);
+            this.button1.Size = new System.Drawing.Size(120, 29);
             this.button1.TabIndex = 0;
             this.button1.Text = "Backup Data";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(394, 202);
+            this.button2.Location = new System.Drawing.Point(272, 251);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(101, 29);
+            this.button2.Size = new System.Drawing.Size(120, 29);
             this.button2.TabIndex = 1;
             this.button2.Text = "Restore Data";
             this.button2.UseVisualStyleBackColor = true;
@@ -54,14 +57,33 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label1.Location = new System.Drawing.Point(165, 54);
+            this.label1.Location = new System.Drawing.Point(79, 20);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(350, 24);
+            this.label1.Size = new System.Drawing.Size(133, 31);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Welcome to Database Management App";
+            this.label1.Text = "Welcome ";
             this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(82, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(465, 160);
+            this.label3.TabIndex = 4;
+            this.label3.Text = resources.GetString("label3.Text");
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // Form1
             // 
@@ -69,6 +91,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.InactiveCaption;
             this.ClientSize = new System.Drawing.Size(717, 351);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -85,6 +109,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- BackupDatabaseApp/Form1.cs
+++ BackupDatabaseApp/Form1.cs
@@ -26,5 +26,10 @@ namespace BackupDatabaseApp
         {
 
         }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
