b1ad5221f4203547e24ffac5c13dc698bbfac592
Gramada Corina <gramadacorina@gmail.com>
2021-06-22 10:25:45 +0300

    Added IEnventsMainDataVolunteerAllocationGateways, IEventsVolunteerAllocationDisplayIndexGateway and EventsMainDisplayVolunteerAllocationContext

---
+++ BucuriaDarului.Core/Gateways/IEnventsMainDataVolunteerAllocationGateways.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BucuriaDarului.Core.Gateways
+{
+    public interface IEnventsMainDataVolunteerAllocationGateways
+    {
+        List<Event> GetListOfEvents();
+        List<Volunteer> GetListOfVolunteers();
+        void UpDateEvent(string eventId,Event eventToUpdate);
+
+    }
+}
---
+++ BucuriaDarului.Gateway/IEventsVolunteerAllocationDisplayIndexGateway.cs
@@ -0,0 +1,43 @@
+﻿using BucuriaDarului.Core;
+using BucuriaDarului.Core.Gateways;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BucuriaDarului.Gateway
+{
+    class IEventsVolunteerAllocationDisplayIndexGateway: IEnventsMainDataVolunteerAllocationGateways
+    {
+        private MongoDBGateway dBContext = new MongoDBGateway();
+
+        public List<Event> GetListOfEvents()
+        {
+            dBContext.ConnectToDB(Connection.SERVER_NAME_LOCAL, Connection.SERVER_PORT_LOCAL, Connection.DATABASE_NAME_LOCAL);
+            IMongoCollection<Event> eventCollection = dBContext.Database.GetCollection<Event>("Events");
+            List<Event> events = eventCollection.AsQueryable().ToList();
+            return events;
+        }
+        public List<Volunteer> GetListOfVolunteers()
+        {
+            dBContext.ConnectToDB(Connection.SERVER_NAME_LOCAL, Connection.SERVER_PORT_LOCAL, Connection.DATABASE_NAME_LOCAL);
+            IMongoCollection<Volunteer> volunteerCollection = dBContext.Database.GetCollection<Volunteer>("Volunteers");
+            List<Volunteer> volunteers = volunteerCollection.AsQueryable().ToList();
+            return volunteers;
+        }
+
+        public void UpDateEvent(string eventId, Event eventToUpdate)
+        {
+            dBContext.ConnectToDB(Connection.SERVER_NAME_LOCAL, Connection.SERVER_PORT_LOCAL, Connection.DATABASE_NAME_LOCAL);
+            IMongoCollection<Event> eventcollection = dBContext.Database.GetCollection<Event>("Events");
+            var filter = Builders<Event>.Filter.Eq("_id", eventId);
+            eventToUpdate._id = eventId;
+            ModifiedIDGateway modifiedIDGateway = new ModifiedIDGateway();
+            modifiedIDGateway.AddIDtoModifications(eventId);
+            eventcollection.FindOneAndReplace(filter, eventToUpdate);
+
+        }
+
+    }
+    }
+}
