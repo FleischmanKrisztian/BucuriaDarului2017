b976569f6e7e3515a4e5d896dd3dd7a2df9f54a3
Gramada Corina <gramadacorina@gmail.com>
2021-06-22 10:43:55 +0300

    Updated Interface

--- BucuriaDarului.Contexts/EventsMainDisplayVolunteerAllocationContext.cs
+++ BucuriaDarului.Contexts/EventsMainDisplayVolunteerAllocationContext.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using BucuriaDarului.Core.Gateways;
 
 namespace BucuriaDarului.Contexts
 {
     class EventsMainDisplayVolunteerAllocationContext
     {
+        private readonly IEnventsMainDataVolunteerAllocationGateways dataGateway;
+
+        public EventsMainDisplayVolunteerAllocationContext(IEnventsMainDataVolunteerAllocationGateways dataGateway)
+        {
+            this.dataGateway = dataGateway;
+        }
+
+
     }
 }
--- BucuriaDarului.Gateway/IEventsVolunteerAllocationDisplayIndexGateway.cs
+++ BucuriaDarului.Gateway/IEventsVolunteerAllocationDisplayIndexGateway.cs
@@ -11,12 +11,14 @@ namespace BucuriaDarului.Gateway
     {
         private MongoDBGateway dBContext = new MongoDBGateway();
 
-        public List<Event> GetListOfEvents()
+        public Event GetEvent(string eventId)
         {
             dBContext.ConnectToDB(Connection.SERVER_NAME_LOCAL, Connection.SERVER_PORT_LOCAL, Connection.DATABASE_NAME_LOCAL);
             IMongoCollection<Event> eventCollection = dBContext.Database.GetCollection<Event>("Events");
-            List<Event> events = eventCollection.AsQueryable().ToList();
-            return events;
+            var filter = Builders<Event>.Filter.Eq("_id", eventId);
+            Event returnevent = eventCollection.Find(filter).FirstOrDefault();
+            return returnevent;
+           
         }
         public List<Volunteer> GetListOfVolunteers()
         {
