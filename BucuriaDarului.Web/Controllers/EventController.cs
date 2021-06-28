2664e620634045fc8082c1f8f3664ed344e19580
Gramada Corina <gramadacorina@gmail.com>
2021-06-28 08:58:04 +0300

    Update EventController.cs

--- BucuriaDarului.Web/Controllers/EventController.cs
+++ BucuriaDarului.Web/Controllers/EventController.cs
@@ -31,7 +31,7 @@ namespace Finalaplication.Controllers
             try
             {
                 var eventsImportContext = new EventsImportContext(new EventsImportDataGateway());
-                eventsImportContext.Execute(file.OpenReadStream());
+                var response=eventsImportContext.Execute(file.OpenReadStream());
                 return RedirectToAction("Index");
             }
             catch
