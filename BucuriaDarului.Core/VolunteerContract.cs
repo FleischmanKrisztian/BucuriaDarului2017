1aed0454efb54895f05111c7e0f3db80b30e5700
Gramada Corina <gramadacorina@gmail.com>
2021-07-08 10:33:15 +0300

    Create VolunteerContract.cs

---
+++ BucuriaDarului.Core/VolunteerContract.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BucuriaDarului.Core
+{
+    internal class VolunteerContract
+    {
+        public string ID { get; set; }
+
+        public string OwnerID { get; set; }
+
+        public string Fullname { get; set; }
+
+        public string CNP { get; set; }
+
+        public string Address { get; set; }
+
+        public int Hourcount { get; set; }
+
+        public string Nrtel { get; set; }
+
+        public CI CI { get; set; }
+
+        public DateTime Birthdate { get; set; }
+
+        public string NumberOfRegistration { get; set; }
+
+        public DateTime RegistrationDate { get; set; }
+
+        public DateTime ExpirationDate { get; set; }
+    }
+}
