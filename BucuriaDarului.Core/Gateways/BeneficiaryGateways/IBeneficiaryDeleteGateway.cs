d4cfbca5e17ba9f753320b34e483daee6d2f1b62
Gramada Corina <gramadacorina@gmail.com>
2021-07-08 10:48:00 +0300

    Created BeneficiaryDeleteGateway and IBeneficiaryDeleteGateway

---
+++ BucuriaDarului.Core/Gateways/BeneficiaryGateways/IBeneficiaryDeleteGateway.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BucuriaDarului.Core.Gateways.BeneficiaryGateways
+{
+    public interface IBeneficiaryDeleteGateway
+    {
+        public void DeleteBeneficiary(string id);
+        public Beneficiary GetVolunteer(string id);
+
+        public void UpdateBeneficiary(string beneficiaryId, Beneficiary beneficiaryToUpdate);
+    }
+}
--- BucuriaDarului.Gateway/BeneficiaryGateways/BeneficiaryDeleteGateway.cs
+++ BucuriaDarului.Gateway/BeneficiaryGateways/BeneficiaryDeleteGateway.cs
@@ -5,13 +5,31 @@ namespace BucuriaDarului.Gateway.BeneficiaryGateways
 {
     public class BeneficiaryDeleteGateway
     {
+        MongoDBGateway dbContext = new MongoDBGateway();
         public static void DeleteBeneficiary(string id)
         {
-            var dbContext = new MongoDBGateway();
+            
             dbContext.ConnectToDB(Connection.SERVER_NAME_LOCAL, Connection.SERVER_PORT_LOCAL, Connection.DATABASE_NAME_LOCAL);
             var beneficiaryCollection = dbContext.Database.GetCollection<Beneficiary>("Beneficiaries");
             var filter = Builders<Beneficiary>.Filter.Eq("_id", id);
             beneficiaryCollection.DeleteOne(filter);
+        }
+
+        public Beneficiary GetVolunteer(string id)
+        {
+            return SingleBeneficiaryReturnerGateway.ReturnBeneficiary(id);
+        }
+
+        public void UpdateBeneficiary(string beneficiaryId, Beneficiary beneficiaryToUpdate)
+        {
+
+            dbContext.ConnectToDB(Connection.SERVER_NAME_LOCAL, Connection.SERVER_PORT_LOCAL, Connection.DATABASE_NAME_LOCAL);
+            var beneficiaryCollection = dbContext.Database.GetCollection<Beneficiary>("Beneficiaries");
+            var filter = Builders<Beneficiary>.Filter.Eq("_id", beneficiaryId);
+            beneficiaryToUpdate.Id = beneficiaryId;
+            var modifiedIDGateway = new ModifiedIDGateway();
+            modifiedIDGateway.AddIDtoModifications(beneficiaryId);
+            beneficiaryCollection.FindOneAndReplace(filter, beneficiaryToUpdate);
         }
     }
 }
