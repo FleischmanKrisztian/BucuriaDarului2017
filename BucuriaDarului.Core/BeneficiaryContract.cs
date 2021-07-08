2b8db56fd6ec103fca21f77ed45c7ac7a16bf11b
Gramada Corina <gramadacorina@gmail.com>
2021-07-08 10:36:31 +0300

    Create BeneficiaryContract.cs

---
+++ BucuriaDarului.Core/BeneficiaryContract.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BucuriaDarului.Core
+{
+    class BeneficiaryContract
+    {
+
+        public string Id { get; set; }
+
+        public string OwnerID { get; set; }
+
+        public string Fullname { get; set; }
+
+        public string NumberOfPortion { get; set; }
+
+        public string CNP { get; set; }
+
+        public string Address { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string CIinfo { get; set; }
+
+        public DateTime Birthdate { get; set; }
+
+        public string NumberOfRegistration { get; set; }
+
+        public DateTime RegistrationDate { get; set; }
+
+        public DateTime ExpirationDate { get; set; }
+
+        public string IdInvestigation { get; set; }
+
+        public string IdApplication { get; set; }
+    }
+}
+    }
+}
