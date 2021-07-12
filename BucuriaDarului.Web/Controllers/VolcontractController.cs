8eeb595167b1d2ad19e375926b45ad24615546e7
Gramada Corina <gramadacorina@gmail.com>
2021-07-12 09:43:12 +0300

    Update VolcontractController.cs

--- BucuriaDarului.Web/Controllers/VolcontractController.cs
+++ BucuriaDarului.Web/Controllers/VolcontractController.cs
@@ -1,37 +1,20 @@
-﻿using Finalaplication.ControllerHelpers.UniversalHelpers;
-using Finalaplication.ControllerHelpers.VolcontractHelpers;
-using Finalaplication.LocalDatabaseManager;
-using Finalaplication.Models;
-using Microsoft.AspNetCore.Mvc;
-using System;
-using BucuriaDarului.Gateway.VolunteerGateways;
-using System.Collections.Generic;
-using System.Linq;
+﻿using BucuriaDarului.Contexts.VolunteerContractContext;
 using BucuriaDarului.Core;
 using BucuriaDarului.Gateway.VolContractGateways;
-using BucuriaDarului.Contexts.VolunteerContractContext;
+using Finalaplication.ControllerHelpers.UniversalHelpers;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Finalaplication.Controllers
 {
     public class VolcontractController : Controller
     {
-        
         [HttpGet]
         public IActionResult Index(string idOfVolunteer)
         {
             var nrOfDocs = UniversalFunctions.GetNumberOfItemPerPageFromSettings(TempData);
-
-            //List<Volcontract> volcontracts = volContractManager.GetListOfVolunteersContracts();
-            //    Volunteer vol = volunteerManager.GetOneVolunteer(idofvol);
-            //    volcontracts = volcontracts.Where(z => z.OwnerID.ToString() == idofvol).ToList();
-            //    ViewBag.nameofvol = vol.Fullname;
-            //    ViewBag.idofvol = idofvol;
-          
             var contractsMainDisplayIndexContext = new VolunteerContractIndexDisplayContext(new VolunteerContractIndexDisplayGateway());
             var model = contractsMainDisplayIndexContext.Execute(new VolunteerContractsMainDisplayIndexRequest(idOfVolunteer, nrOfDocs));
             return View(model);
-            
-            
         }
 
         public ActionResult ContractExp()
@@ -39,7 +22,6 @@ namespace Finalaplication.Controllers
             var contractExpirationContext = new VolunteerContractsExpirationContext(new VolunteerContractExpirationGateway());
             var contracts = contractExpirationContext.Execute();
             return View(contracts);
-           
         }
 
         [HttpGet]
@@ -47,44 +29,41 @@ namespace Finalaplication.Controllers
         {
             ViewBag.message = message;
             ViewBag.idOfVol = idOfVolunteer;
-                return View();
+            return View();
         }
 
         [HttpPost]
         public ActionResult Create(VolunteerContract volunteerContract, string idOfVolunteer)
         {
             var contractCreateContext = new VolunteerContractCreateContext(new VolunteerContractCreateGateway());
-            var contractCreateResponse = contractCreateContext.Execute(volunteerContract,  idOfVolunteer);
+            var contractCreateResponse = contractCreateContext.Execute(volunteerContract, idOfVolunteer);
 
             if (!contractCreateResponse.IsValid)
             {
                 return RedirectToAction("Create", new { idOfVolunteer = idOfVolunteer, message = contractCreateResponse.Message });
             }
             return RedirectToAction("Index", new { idOfVolunteer = idOfVolunteer });
-
         }
 
         [HttpGet]
         public ActionResult Print(string id)
         {
             var model = SingleVolunteerContractReturnerGateway.GetVolunteerContract(id);
-                return View(model);
+            return View(model);
         }
 
         [HttpGet]
         public ActionResult Delete(string id)
         {
-                var model= SingleVolunteerContractReturnerGateway.GetVolunteerContract(id);
-                return View(model);
-            
+            var model = SingleVolunteerContractReturnerGateway.GetVolunteerContract(id);
+            return View(model);
         }
 
         [HttpPost]
         public ActionResult Delete(string id, string idOfVol)
         {
-                VolunteerContractDeleteGateway.Delete(id);
-                return RedirectToAction("Index", new { idOfVolunteer= idOfVol });
-            
+            VolunteerContractDeleteGateway.Delete(id);
+            return RedirectToAction("Index", new { idOfVolunteer = idOfVol });
         }
     }
 }
