b76fc7318a3f2320a93870c9be92c846cc822127
Gramada Corina <gramadacorina@gmail.com>
2021-07-12 09:58:08 +0300

    Create ISettingsGateway.cs

---
+++ BucuriaDarului.Core/Gateways/SettingsGateways/ISettingsGateway.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BucuriaDarului.Core.Gateways.SettingsGateways
+{
+    public interface ISettingsGateway
+    {
+        Settings GetSettings();
+        void UpdateSettings(Settings settings);
+    }
+}
