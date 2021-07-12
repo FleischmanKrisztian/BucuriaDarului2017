984c49e8d442fe8d79e761afd472f265fcfd6478
Gramada Corina <gramadacorina@gmail.com>
2021-07-12 18:04:06 +0300

    Cleaned ControllerHelper

--- BucuriaDarului.Web/Common/ControllerHelper.cs
+++ BucuriaDarului.Web/Common/ControllerHelper.cs
@@ -33,43 +33,7 @@ namespace BucuriaDarului.Web.Common
             return result;
         }
 
-        public static string GetHeaderForExcelPrinterVolunteer(IStringLocalizer<VolunteerController> _localizer)
-        {
-            string[] header = new string[25];
-            header[0] = _localizer["Fullname"];
-            header[1] = _localizer["Birthdate"];
-            header[2] = _localizer["Address"];
-            header[3] = _localizer["Gender"];
-            header[4] = _localizer["DesiredArea"];
-            header[5] = _localizer["CNP"];
-            header[6] = _localizer["FieldOfActivity"];
-            header[7] = _localizer["Occupation"];
-            header[8] = _localizer["CI_Info"];
-            header[9] = _localizer["Active"];
-            header[10] = _localizer["HourCount"];
-            header[11] = _localizer["ContactInfo"];
-            header[12] = _localizer["Additional_info"];
-            header[14] = _localizer["Address"];
-            header[18] = _localizer["CISeria"];
-            header[19] = _localizer["CINr"];
-            header[20] = _localizer["CIEliberat"];
-            header[21] = _localizer["CIeliberator"];
-            header[20] = _localizer["PhoneNumber"];
-            header[21] = _localizer["EmailAddress"];
-            header[22] = _localizer["HasDrivingLicense"];
-            header[23] = _localizer["HasCar"];
-            header[24] = _localizer["Remarks"];
-
-            string result = string.Empty;
-            for (int i = 0; i < header.Count(); i++)
-            {
-                if (i == 0)
-                { result = header[i]; }
-                else
-                { result = result + "," + header[i]; }
-            }
-            return result;
-        }
+       
 
         public static string GetHeaderForExcelPrinterSponsor(IStringLocalizer<SponsorController> _localizer)
         {
@@ -94,55 +58,6 @@ namespace BucuriaDarului.Web.Common
             return result;
         }
 
-        public static string GetHeaderForExcelPrinterBeneficiary(IStringLocalizer<BeneficiaryController> _localizer)
-        {
-            string[] header = new string[35];
-            header[0] = _localizer["Fullname"];
-            header[1] = _localizer["Active"];
-            header[2] = _localizer["Canteen"];
-            header[3] = _localizer["HomeDelivery"];
-            header[4] = _localizer["HomeDeliveryDriver"];
-            header[5] = _localizer["HasGDPRAgreement"];
-            header[6] = _localizer["AddressInformation"];
-            header[7] = _localizer["CNP"];
-            header[8] = _localizer["CISeria"];
-            header[9] = _localizer["CIEliberat"];
-            header[10] = _localizer["Marca"];
-            header[11] = _localizer["IdInvestigation"];
-            header[12] = _localizer["IdApplication"];
-            header[13] = _localizer["NumberOfPortions"];
-            header[14] = _localizer["Lastimeactiv"];
-            header[15] = _localizer["PhoneNumber"];
-            header[16] = _localizer["Birthplace"];
-            header[17] = _localizer["Studies"];
-            header[18] = _localizer["Profession"];
-            header[19] = _localizer["Occupation"];
-            header[20] = _localizer["SeniorityInWorkField"];
-            header[21] = _localizer["HealthState"];
-            header[22] = _localizer["Disability"];
-            header[23] = _localizer["ChronicCondition"];
-            header[24] = _localizer["Addictions"];
-            header[25] = _localizer["HasHealthInsurance"];
-            header[26] = _localizer["HasHealthCard"];
-            header[27] = _localizer["Married"];
-            header[28] = _localizer["SpouseName"];
-            header[29] = _localizer["Homeless"];
-            header[30] = _localizer["HousingType"];
-            header[31] = _localizer["Income"];
-            header[32] = _localizer["Expenses"];
-            header[33] = _localizer["Gender"];
-            header[34] = _localizer["WeeklyPackage"];
-
-            string result = string.Empty;
-            for (int i = 0; i < header.Count(); i++)
-            {
-                if (i == 0)
-                { result = header[i]; }
-                else
-                { result = result + "," + header[i]; }
-            }
-            return result;
-        }
 
         internal static void CreateDictionaries(string key1, string key2, string ids_and_fields, string header)
         {
