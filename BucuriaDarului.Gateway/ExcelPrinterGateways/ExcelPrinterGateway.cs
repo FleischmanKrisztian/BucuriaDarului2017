b1e3df567d629c0d9cad456f7b2adab6c5040097
Gramada Corina <gramadacorina@gmail.com>
2021-07-13 10:34:06 +0300

    Added a static method for GetListVolunteer

---
+++ BucuriaDarului.Gateway/ExcelPrinterGateways/ExcelPrinterGateway.cs
@@ -0,0 +1,32 @@
+﻿using BucuriaDarului.Core;
+using BucuriaDarului.Core.Gateways.ExcelPrinterGateways;
+using BucuriaDarului.Gateway.SponsorGateways;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BucuriaDarului.Gateway.ExcelPrinterGateways
+{
+    public class ExcelPrinterGateway : IExcelPrinterGateway
+    {
+        public List<Beneficiary> GetListOfBeneficiary()
+        {
+            return;
+        }
+
+        public List<Event> GetListOfEvents()
+        {
+           return;
+        }
+
+        public List<Sponsor> GetListOfSponsors()
+        {
+           return ListSponsorsGateway.GetListOfSponsors();
+        }
+
+        public List<Volunteer> GetListOfVolunteerss()
+        {
+            return;
+        }
+    }
+}
--- BucuriaDarului.Gateway/VolunteerGateways/ListVolunteerGateway.cs
+++ BucuriaDarului.Gateway/VolunteerGateways/ListVolunteerGateway.cs
@@ -1,16 +1,16 @@
 ﻿using BucuriaDarului.Core;
-using BucuriaDarului.Core.Gateways.VolunteerGateways;
 using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace BucuriaDarului.Gateway.VolunteerGateways
 {
-    public class ListVolunteersGateway : IListDisplayVolunteersGateway
+    public class ListVolunteerGateway
     {
-        private MongoDBGateway dbContext = new MongoDBGateway();
-
-        public List<Volunteer> GetListOfVolunteers()
+        public static List<Volunteer> GetListOfVolunteers()
         {
+            var dbContext = new MongoDBGateway();
             dbContext.ConnectToDB(Connection.SERVER_NAME_LOCAL, Connection.SERVER_PORT_LOCAL, Connection.DATABASE_NAME_LOCAL);
             var volunteerCollection = dbContext.Database.GetCollection<Volunteer>("Volunteers");
             List<Volunteer> volunteers = volunteerCollection.AsQueryable().ToList();
