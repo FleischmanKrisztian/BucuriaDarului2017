8fdba2023b91f31e01c2afe9b9d7e7a1dd93bd31
Gramada Corina <gramadacorina@gmail.com>
2021-07-13 20:46:29 +0300

    Update SponsorFunctions.cs

--- BucuriaDarului.Web/ControllerHelpers/SponsorHelpers/SponsorFunctions.cs
+++ BucuriaDarului.Web/ControllerHelpers/SponsorHelpers/SponsorFunctions.cs
@@ -25,36 +25,6 @@ namespace BucuriaDarului.Web.ControllerHelpers.SponsorHelpers
             return sponsors;
         }
 
-        internal static List<Sponsor> GetSponsorsAfterSearching(List<Sponsor> sponsors, string searching)
-        {
-            if (searching != null)
-            {
-                sponsors = sponsors.Where(x => x.NameOfSponsor.Contains(searching, StringComparison.InvariantCultureIgnoreCase)).ToList();
-            }
-            return sponsors;
-        }
-
-        internal static List<Sponsor> GetSponsorsByIds(List<Sponsor> sponsors, string[] sponsorids)
-        {
-            List<Sponsor> sponsorlist = new List<Sponsor>();
-            for (int i = 0; i < sponsorids.Length; i++)
-            {
-                Sponsor singlesponsor = sponsors.Where(x => x.Id == sponsorids[i]).First();
-                sponsorlist.Add(singlesponsor);
-            }
-            return sponsorlist;
-        }
-
-        internal static string GetSponsorNames(List<Sponsor> sponsors)
-        {
-            string sponsornames = "";
-            for (int i = 0; i < sponsors.Count; i++)
-            {
-                var sponsor = sponsors[i];
-                sponsornames = sponsornames + sponsor.NameOfSponsor + " / ";
-            }
-            return sponsornames;
-        }
 
         private static bool Dateinputreceived(DateTime date)
         {
