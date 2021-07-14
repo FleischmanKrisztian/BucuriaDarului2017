17b1340d438ed97c56bc192dc8486fda36ff361e
FleischmanKrisztian <49558830+FleischmanKrisztian@users.noreply.github.com>
2021-07-14 09:59:37 +0300

    Changed Wpfappname and attempt at publishing

--- wpfapp/Properties/Resources.Designer.cs
+++ wpfapp/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace wpfapp.Properties {
+namespace ContractPrinter.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace wpfapp.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("wpfapp.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ContractPrinter.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
