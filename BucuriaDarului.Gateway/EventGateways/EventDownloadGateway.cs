44f5eab367003dde53becbe32e42549386802f98
Gramada Corina <gramadacorina@gmail.com>
2021-07-15 17:07:02 +0300

    Create EventDownloadGateway.cs

---
+++ BucuriaDarului.Gateway/EventGateways/EventDownloadGateway.cs
@@ -0,0 +1,16 @@
+﻿using BucuriaDarului.Core;
+using BucuriaDarului.Core.Gateways.EventGateways;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BucuriaDarului.Gateway.EventGateways
+{
+    public class EventDownloadGateway : IEventDownloadGateway
+    {
+        public List<Event> GetListOfEvents()
+        {
+            return ListEventsGateway.GetListOfEvents();
+        }
+    }
+}
