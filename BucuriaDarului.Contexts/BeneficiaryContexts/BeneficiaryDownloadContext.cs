2a0c75feb0d67243960a526a4e39e2c87c8c3288
Gramada Corina <gramadacorina@gmail.com>
2021-07-19 12:44:42 +0300

    Download button for Beneficiary Csv works

--- BucuriaDarului.Contexts/BeneficiaryContexts/BeneficiaryDownloadContext.cs
+++ BucuriaDarului.Contexts/BeneficiaryContexts/BeneficiaryDownloadContext.cs
@@ -25,8 +25,8 @@ namespace BucuriaDarului.Contexts.BeneficiaryContexts
 
             if (properties.Contains("0"))
                 jsonString = GetBneficiariesCsv(ids, finalHeader);
-            //else
-            //jsonString = GetBeneficiary(properties, ids, finalHeader);
+            else
+                jsonString = GetBeneficiaries(properties, ids, finalHeader);
 
             return jsonString;
         }
@@ -115,7 +115,7 @@ namespace BucuriaDarului.Contexts.BeneficiaryContexts
                 sb.Append("\"" + data + "\"" + ",");
             }
             sb.Append("\r\n");
-            
+
             foreach (var item in finalListOfBeneficiaries)
             {
                 string[] arrbeneficiary = BeneficiaryToArray(item);
@@ -136,94 +136,312 @@ namespace BucuriaDarului.Contexts.BeneficiaryContexts
         {
             List<string> headerList = new List<string>();
             if (properties.Contains("1"))
-
+            {
                 headerList.Add(header[1]);
-
+            }
             if (properties.Contains("2"))
-
+            {
                 headerList.Add(header[2]);
-
+            }
             if (properties.Contains("3"))
-
-                headerList.Add(header[3]);
-
-            if (properties.Contains("4"))
+            {
                 headerList.Add(header[4]);
-
-            if (properties.Contains("5"))
+            }
+            if (properties.Contains("Z"))
+            {
+                headerList.Add(header[3]);
+            }
+            if (properties.Contains("4"))
+            {
                 headerList.Add(header[5]);
-
-            if (properties.Contains("6"))
-
+            }
+            if (properties.Contains("5"))
+            {
                 headerList.Add(header[6]);
-
-            if (properties.Contains("7"))
-
+            }
+            if (properties.Contains("6"))
+            {
                 headerList.Add(header[7]);
-
+            }
+            if (properties.Contains("7"))
+            {
+                headerList.Add(header[9]);
+            }
             if (properties.Contains("8"))
-                headerList.Add(header[8]);
+            {
+                headerList.Add(header[9]);
+            }
+            if (properties.Contains("9"))
+            {
+                headerList.Add(header[11]);
+            }
+            if (properties.Contains("A"))
+            {
+                headerList.Add(header[13]);
+            }
+            if (properties.Contains("B"))
+            {
+                headerList.Add(header[15]);
+            }
+            if (properties.Contains("C"))
+            {
+                headerList.Add(header[14]);
+            }
+            if (properties.Contains("D"))
+            {
+                headerList.Add(header[17]);
+            }
+            if (properties.Contains("E"))
+            {
+                headerList.Add(header[18]);
+            }
+            if (properties.Contains("F"))
+            {
+                headerList.Add(header[21]);
+            }
+            if (properties.Contains("G"))
+            {
+                headerList.Add(header[20]);
+            }
+            if (properties.Contains("H"))
+            {
+                headerList.Add(header[22]);
+            }
+            if (properties.Contains("I"))
+            {
+                headerList.Add(header[23]);
+            }
+            if (properties.Contains("J"))
+            {
+                headerList.Add(header[24]);
+            }
+            if (properties.Contains("K"))
+            {
+                headerList.Add(header[25]);
+            }
+
+            if (properties.Contains("L"))
+            {
+                headerList.Add(header[26]);
+            }
+            if (properties.Contains("M"))
+            {
+                headerList.Add(header[27]);
+            }
+            if (properties.Contains("N"))
+            {
+                headerList.Add(header[28]);
+            }
+            if (properties.Contains("O"))
+            {
+                headerList.Add(header[29]);
+            }
+            if (properties.Contains("Z"))
+            {
+                headerList.Add(header[30]);
+            }
+            if (properties.Contains("P"))
+            {
+                headerList.Add(header[31]);
+            }
+            if (properties.Contains("Q"))
+            {
+                headerList.Add(header[32]);
+            }
+            if (properties.Contains("R"))
+            {
+                headerList.Add(header[33]);
+            }
+            if (properties.Contains("S"))
+            {
+                headerList.Add(header[34]);
+            }
+            if (properties.Contains("T"))
+            {
+                headerList.Add(header[35]);
+            }
+            if (properties.Contains("U"))
+            {
+                headerList.Add(header[36]);
+            }
+            if (properties.Contains("V"))
+            {
+                headerList.Add(header[37]);
+            }
+            if (properties.Contains("W"))
+            {
+                headerList.Add(header[38]);
+            }
 
             var returnedHeader = headerList.ToArray();
             return returnedHeader;
         }
 
-        //public string GetBeneficiaries(string properties, string[] ids, string[] header)
-        //{
-        //    var usedHeader = GetUsedHeader(properties, header);
-        //    var listOfBeneficiary = dataGateway.GetListOfBeneficiaries();
-        //    var idListForPrinting = new List<Beneficiary>();
-        //    foreach (var b in listOfBeneficiary)
-        //    {
-        //        if (ids.Contains(b.Id))
-        //        {
-        //            idListForPrinting.Add(b);
-        //        }
-        //    }
+        public string GetBeneficiaries(string properties, string[] ids, string[] header)
+        {
+            var usedHeader = GetUsedHeader(properties, header);
+            var listOfBeneficiary = dataGateway.GetListOfBeneficiaries();
+            var idListForPrinting = new List<Beneficiary>();
+            foreach (var b in listOfBeneficiary)
+            {
+                if (ids.Contains(b.Id))
+                {
+                    idListForPrinting.Add(b);
+                }
+            }
 
-        //    StringBuilder sb = new StringBuilder();
-        //    foreach (var data in usedHeader)
-        //    {
-        //        sb.Append("\"" + data + "\"" + ",");
-        //    }
+            StringBuilder sb = new StringBuilder();
+            foreach (var data in usedHeader)
+            {
+                sb.Append("\"" + data + "\"" + ",");
+            }
 
-        //    sb.Append("\r\n");
-        //    foreach (var item in idListForPrinting)
-        //    {
-        //        string[] arrBeneficiary = BeneficiaryToArray(item);
+            sb.Append("\r\n");
+            foreach (var item in idListForPrinting)
+            {
+                string[] arrBeneficiary = BeneficiaryToArray(item);
 
-        //        if (properties.Contains("1"))
+                if (properties.Contains("1"))
+                {
+                    sb.Append("\"" + item.Fullname + "\"" + ",");
+                }
+                if (properties.Contains("2"))
+                {
+                    sb.Append("\"" + item.Active + "\"" + ",");
+                }
 
-        //            sb.Append("\"" + arrBeneficiary[1] + "\"" + ",");
+                if (properties.Contains("3"))
+                {
+                    sb.Append("\"" + item.Canteen + "\"" + ",");
+                }
+                if (properties.Contains("Z"))
+                {
+                    sb.Append("\"" + item.WeeklyPackage + "\"" + ",");
+                }
+                if (properties.Contains("4"))
+                {
+                    sb.Append("\"" + item.HomeDelivery + "\"" + ",");
+                }
+                if (properties.Contains("5"))
+                {
+                    sb.Append("\"" + item.HomeDeliveryDriver + "\"" + ",");
+                }
+                if (properties.Contains("6"))
+                {
+                    sb.Append("\"" + item.HasGDPRAgreement + "\"" + ",");
+                }
+                if (properties.Contains("7"))
+                {
+                    sb.Append("\"" + item.Address + "\"" + ",");
+                }
+                if (properties.Contains("8"))
+                {
+                    sb.Append("\"" + item.CNP + "\"" + ",");
+                }
+                if (properties.Contains("9"))
+                {
+                    sb.Append("\"" + item.CI.Info + "\"" + ",");
+                }
+                if (properties.Contains("A"))
+                {
+                    sb.Append("\"" + item.Marca.MarcaName + "\"" + ",");
+                }
+                if (properties.Contains("B"))
+                {
+                    sb.Append("\"" + item.Marca.IdInvestigation + "\"" + ",");
+                }
+                if (properties.Contains("C"))
+                {
+                    sb.Append("\"" + item.Marca.IdApplication + "\"" + ",");
+                }
+                if (properties.Contains("D"))
+                {
+                    sb.Append("\"" + item.NumberOfPortions + "\"" + ",");
+                }
+                if (properties.Contains("E"))
+                {
+                    sb.Append("\"" + item.LastTimeActive.ToLongDateString() + "\"" + ",");
+                }
+                if (properties.Contains("F"))
+                {
+                    sb.Append("\"" + item.PersonalInfo.PhoneNumber + "\"" + ",");
+                }
+                if (properties.Contains("G"))
+                {
+                    sb.Append("\"" + item.PersonalInfo.BirthPlace + "\"" + ",");
+                }
+                if (properties.Contains("H"))
+                {
+                    sb.Append("\"" + item.PersonalInfo.Studies + "\"" + ",");
+                }
+                if (properties.Contains("I"))
+                {
+                    sb.Append("\"" + item.PersonalInfo.Profession + "\"" + ",");
+                }
+                if (properties.Contains("J"))
+                {
+                    sb.Append("\"" + item.PersonalInfo.Occupation + "\"" + ",");
+                }
+                if (properties.Contains("K"))
+                {
+                    sb.Append("\"" + item.PersonalInfo.SeniorityInWorkField + "\"" + ",");
+                }
+                if (properties.Contains("L"))
+                {
+                    sb.Append("\"" + item.PersonalInfo.HealthState + "\"" + ",");
+                }
+                if (properties.Contains("M"))
+                {
+                    sb.Append("\"" + item.PersonalInfo.Disability + "\"" + ",");
+                }
+                if (properties.Contains("N"))
+                {
+                    sb.Append("\"" + item.PersonalInfo.ChronicCondition + "\"" + ",");
+                }
+                if (properties.Contains("O"))
+                {
+                    sb.Append("\"" + item.PersonalInfo.Addictions + "\"" + ",");
+                }
+                if (properties.Contains("Z"))
+                {
+                    sb.Append("\"" + item.PersonalInfo.HealthInsurance + "\"" + ",");
+                }
+                if (properties.Contains("P"))
+                {
+                    sb.Append("\"" + item.PersonalInfo.HealthCard + "\"" + ",");
+                }
+                if (properties.Contains("Q"))
+                {
+                    sb.Append("\"" + item.PersonalInfo.Married + "\"" + ",");
+                }
+                if (properties.Contains("R"))
+                {
+                    sb.Append("\"" + item.PersonalInfo.SpouseName + "\"" + ",");
+                }
+                if (properties.Contains("S"))
+                {
+                    sb.Append("\"" + item.PersonalInfo.HasHome + "\"" + ",");
+                }
+                if (properties.Contains("T"))
+                {
+                    sb.Append("\"" + item.PersonalInfo.HousingType + "\"" + ",");
+                }
+                if (properties.Contains("U"))
+                {
+                    sb.Append("\"" + item.PersonalInfo.Income + "\"" + ",");
+                }
+                if (properties.Contains("V"))
+                {
+                    sb.Append("\"" + item.PersonalInfo.Expenses + "\"" + ",");
+                }
+                if (properties.Contains("W"))
+                {
+                    sb.Append("\"" + item.PersonalInfo.Gender + "\"" + ",");
+                }
 
-        //        if (properties.Contains("2"))
-
-        //            sb.Append("\"" + arrBeneficiary[2] + "\"" + ",");
-
-        //        if (properties.Contains("3"))
-
-        //            sb.Append("\"" + arrBeneficiary[3] + "\"" + ",");
-
-        //        if (properties.Contains("4"))
-        //            sb.Append("\"" + arrBeneficiary[4] + "\"" + ",");
-
-        //        if (properties.Contains("5"))
-        //            sb.Append("\"" + arrBeneficiary[5] + "\"" + ",");
-
-        //        if (properties.Contains("6"))
-
-        //            sb.Append("\"" + arrBeneficiary[6] + "\"" + ",");
-
-        //        if (properties.Contains("7"))
-
-        //            sb.Append("\"" + arrBeneficiary[7] + "\"" + ",");
-
-        //        if (properties.Contains("8"))
-        //            sb.Append("\"" + arrBeneficiary[8] + "\"" + ",");
-
-        //        sb.Append("\r\n");
-        //    }
-        //    return sb.ToString();
-        //}
+                sb.Append("\r\n");
+            }
+            return sb.ToString();
+        }
     }
 }
