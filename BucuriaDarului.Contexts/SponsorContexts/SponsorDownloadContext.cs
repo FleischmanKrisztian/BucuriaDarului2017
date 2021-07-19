fd0656d5bcc4c53b792c20874e72d900a240a314
Gramada Corina <gramadacorina@gmail.com>
2021-07-19 16:48:44 +0300

    Create SponsorDownloadContext.cs

---
+++ BucuriaDarului.Contexts/SponsorContexts/SponsorDownloadContext.cs
@@ -0,0 +1,17 @@
+﻿using BucuriaDarului.Core.Gateways.SponsorGateways;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BucuriaDarului.Contexts.SponsorContexts
+{
+    public class SponsorDownloadContext
+    {
+        private readonly ISponsorDownloadGateway dataGateway;
+
+        public SponsorDownloadContext(ISponsorDownloadGateway dataGateway)
+        {
+            this.dataGateway = dataGateway;
+        }
+    }
+}
