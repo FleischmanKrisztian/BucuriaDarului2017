d377919f54e0ba264d791e1d4a384ff683cdb883
Gramada Corina <gramadacorina@gmail.com>
2021-07-19 16:48:22 +0300

    Create ISponsorDownloadGateway.cs

---
+++ BucuriaDarului.Core/Gateways/SponsorGateways/ISponsorDownloadGateway.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BucuriaDarului.Core.Gateways.SponsorGateways
+{
+   public interface ISponsorDownloadGateway
+    {
+        List<Sponsor> GetListOfSponsors();
+    }
+}
