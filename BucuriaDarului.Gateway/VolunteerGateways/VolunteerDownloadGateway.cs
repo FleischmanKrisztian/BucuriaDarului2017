c07d1598f5e00e28ac7149c4c242f8a980bf9370
Gramada Corina <gramadacorina@gmail.com>
2021-07-20 17:05:43 +0300

    Create VolunteerDownloadGateway.cs

---
+++ BucuriaDarului.Gateway/VolunteerGateways/VolunteerDownloadGateway.cs
@@ -0,0 +1,16 @@
+﻿using BucuriaDarului.Core;
+using BucuriaDarului.Core.Gateways.VolunteerGateways;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BucuriaDarului.Gateway.VolunteerGateways
+{
+    public class VolunteerDownloadGateway : IVolunteerDownloadGateway
+    {
+        public List<Volunteer> GetListOfVolunteers()
+        {
+            return ListVolunteersGateway.GetListOfVolunteers();
+        }
+    }
+}
