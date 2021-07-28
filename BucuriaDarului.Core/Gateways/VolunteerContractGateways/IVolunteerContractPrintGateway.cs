997bd65b07498903ccde503f64b4746f42734971
Gramada Corina <gramadacorina@gmail.com>
2021-07-28 21:48:43 +0300

    Create IVolunteerContractPrintGateway.cs

---
+++ BucuriaDarului.Core/Gateways/VolunteerContractGateways/IVolunteerContractPrintGateway.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BucuriaDarului.Core.Gateways.VolunteerContractGateways
+{
+    public interface IVolunteerContractPrintGateway
+    {
+        VolunteerContract GetVolunteerContract(string idContract);
+    }
+}
