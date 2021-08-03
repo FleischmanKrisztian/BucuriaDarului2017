0cf6812dd1f61fcde3b407d63418e6c03a09f31b
corinagramada <45661319+corinagramada@users.noreply.github.com>
2021-08-03 17:41:01 +0300

    Create IBeneficiaryContractPrintGateway.cs

---
+++ BucuriaDarului.Core/Gateways/BeneficiaryContractGateways/IBeneficiaryContractPrintGateway.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BucuriaDarului.Core.Gateways.BeneficiaryContractGateways
+{
+    public interface IBeneficiaryContractPrintGateway
+    {
+        BeneficiaryContract GetBeneficiaryContract(string beneficiaryContractId);
+    }
+}
