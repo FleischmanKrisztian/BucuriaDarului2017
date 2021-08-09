a2559e5cffd61452504545b159b0c62f29009f29
corinagramada <45661319+corinagramada@users.noreply.github.com>
2021-08-09 12:57:08 +0300

    Update FirstStartupContext.cs

--- BucuriaDarului.Contexts/SettingsContexts/FirstStartupContext.cs
+++ BucuriaDarului.Contexts/SettingsContexts/FirstStartupContext.cs
@@ -7,7 +7,7 @@ namespace BucuriaDarului.Contexts.SettingsContexts
         public FirstStartupResponse Execute()
         {
             var setting = SingleSettingReturnerGateway.GetSettingItem();
-            var response = new FirstStartupResponse(setting.Lang, setting.Quantity);
+            var response = new FirstStartupResponse(setting.Lang, setting.Quantity, setting.NumberOfDaysBeforBirthday, setting.NumberOfDaysBeforeExpiration);
             return response;
         }
 
@@ -15,11 +15,15 @@ namespace BucuriaDarului.Contexts.SettingsContexts
         {
             public string Language { get; set; }
             public int NumberOfItemsPerPage { get; set; }
+            public int NumberOfDaysBeforBirthday { get; set; }
+            public int NumberOfDaysBeforeExpiration { get; set; }
 
-            public FirstStartupResponse(string language, int numberOfItemsPerPage)
+            public FirstStartupResponse(string language, int numberOfItemsPerPage, int numberOfDaysBeforBirthday, int numberOfDaysBeforeExpiration)
             {
                 Language = language;
                 NumberOfItemsPerPage = numberOfItemsPerPage;
+                NumberOfDaysBeforBirthday = numberOfDaysBeforBirthday;
+                NumberOfDaysBeforeExpiration = numberOfDaysBeforeExpiration;
             }
         }
     }
