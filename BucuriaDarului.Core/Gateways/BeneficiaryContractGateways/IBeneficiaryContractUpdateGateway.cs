760b81d31f3b85e45531de5acf2be8bb85d94f52
corinagramada <45661319+corinagramada@users.noreply.github.com>
2021-08-17 10:46:45 +0300

    Create IBeneficiaryContractUpdateGateway.cs

---
+++ BucuriaDarului.Core/Gateways/BeneficiaryContractGateways/IBeneficiaryContractUpdateGateway.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BucuriaDarului.Core.Gateways.BeneficiaryContractGateways
+{
+    public interface IBeneficiaryContractUpdateGateway
+    {
+        Beneficiary GetBeneficiary(string beneficiaryId);
+        List<BeneficiaryContract> GetContractsOfBeneficiary();
+        void AddBeneficiaryContractToModifiedList(string beforeEditingBeneficiaryContractString);
+        public List<ModifiedIDs> ReturnModificationList();
+        void UpdateBeneficiaryContract(BeneficiaryContract beneficiaryContract);
+    }
+}
