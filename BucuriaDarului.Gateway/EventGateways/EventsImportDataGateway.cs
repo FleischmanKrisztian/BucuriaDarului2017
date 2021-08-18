4287123277e9cf61a6da512612dcc73f884bb368
corinagramada <45661319+corinagramada@users.noreply.github.com>
2021-08-18 17:36:51 +0300

    Update EventsImportDataGateway.cs

--- BucuriaDarului.Gateway/EventGateways/EventsImportDataGateway.cs
+++ BucuriaDarului.Gateway/EventGateways/EventsImportDataGateway.cs
@@ -9,6 +9,16 @@ namespace BucuriaDarului.Gateway.EventGateways
     {
         private readonly MongoDBGateway dbContext = new MongoDBGateway();
 
+        public  Event GetEvent(string eventId)
+        {
+            return SingleEventReturnerGateway.ReturnEvent(eventId);
+        }
+
+        public List<Event> GetEvents()
+        {
+            return ListEventsGateway.GetListOfEvents();
+        }
+
         public void Insert(List<Event> events)
         {
             dbContext.ConnectToDB(Connection.SERVER_NAME_LOCAL, Connection.SERVER_PORT_LOCAL, Connection.DATABASE_NAME_LOCAL);
@@ -22,6 +32,19 @@ namespace BucuriaDarului.Gateway.EventGateways
                     eventCollection.InsertOne(eve);
                     modifiedIdGateway.AddIDtoModifications(eve.Id);
                 }
+            }
+        }
+
+        public void Update(List<Event> events)
+        {
+            foreach (var @event in events)
+            {
+                dbContext.ConnectToDB(Connection.SERVER_NAME_LOCAL, Connection.SERVER_PORT_LOCAL, Connection.DATABASE_NAME_LOCAL);
+                var eventCollection = dbContext.Database.GetCollection<Event>("Events");
+                var filter = Builders<Event>.Filter.Eq("Id", @event.Id);
+                var modifiedIdGateway = new ModifiedIDGateway();
+                modifiedIdGateway.AddIDtoModifications(@event.Id);
+                eventCollection.FindOneAndReplace(filter, @event);
             }
         }
     }
