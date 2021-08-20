808dcc4fe0584dd323c485a33afb021d307afe97
corinagramada <45661319+corinagramada@users.noreply.github.com>
2021-08-20 17:33:07 +0300

    Create ISponsorDeleteGateway.cs

---
+++ BucuriaDarului.Core/Gateways/SponsorGateways/ISponsorDeleteGateway.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace BucuriaDarului.Core.Gateways.SponsorGateways
+{
+    public interface ISponsorDeleteGateway
+    {
+        void DeleteSponsor(string id);
+
+        Sponsor GetSponsor(string sponsorId);
+
+        List<Event> GetEvents();
+
+        Event GetEvent(string idEvent);
+
+        void UpdateEvent(string eventId, Event eventToUpdate);
+    }
+}
